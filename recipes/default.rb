8a4cb076bf6e8755a721e12f7ac3e467ab1ff9e4
Tyler van Hensbergen <tvanhens@gmail.com>
2014-02-26 15:41:48 -0500

    Added ark cookbook dependency

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,21 +1,3 @@
 include_recipe 'apt'
 include_recipe 'java'
-
-group "dynamodb-local" do
-end
-
-user "dynamodb-local" do
-  gid "dynamodb-local"
-end
-
-remote_file "tmp/dynamodb-local.tar.gz" do
-  source "https://s3-us-west-2.amazonaws.com/dynamodb-local/dynamodb_local_2013-12-12.tar.gz"
-  user "root"
-  group "root"
-  backup false
-end
-
-bash "extract dynamodb local" do
-  cwd "/tmp/"
-  code "tar -zxvf dynamodb-local.tar.gz && cp -fr dynamodb_local_2013-12-12/ /home/dynamodb-local/"
-end
+include_recipe 'ark'
