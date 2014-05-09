78332a2fa266b9a9b8521b365f87cc8accf19ba6
Maggie Nelson <maggie@maggienelson.com>
2014-05-09 13:36:53 -0700

    Tell ark to install in a directory specified by :path

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,7 +5,8 @@ include_recipe 'runit'
 
 ark 'dynamodb' do
   url node[:dynamodb_local][:source]
-  extension 'tar.gz'
+  path node[:dynamodb_local][:path]
+  action :install
 end
 
 runit_service 'dynamodb-local' do
@@ -13,6 +14,7 @@ runit_service 'dynamodb-local' do
   options({
     :dynamodb_local_port => node[:dynamodb_local][:port],
     :dynamodb_local_log_dir => node[:dynamodb_local][:log_dir],
+    :dynamodb_local_path => node[:dynamodb_local][:path],
     :dynamodb_local_java_library_path => node[:dynamodb_local][:java_library_path],
     :dynamodb_local_jar => node[:dynamodb_local][:jar],
     :dynamodb_local_db_path => node[:dynamodb_local][:db_path]
