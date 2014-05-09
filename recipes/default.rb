c25bf21550b31eeacc8c4eb9af9e697497d41efc
Maggie Nelson <maggie@maggienelson.com>
2014-05-09 15:57:24 -0700

    referencing node properly

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,7 +16,7 @@ runit_service 'dynamodb-local' do
     :dynamodb_local_port => node[:dynamodb_local][:port],
     :dynamodb_local_log_dir => node[:dynamodb_local][:log_dir],
     :dynamodb_local_path => node[:dynamodb_local][:path],
-    :dynamodb_local_strip_components => [:dynamodb_local][:strip_components],
+    :dynamodb_local_strip_components => node[:dynamodb_local][:strip_components],
     :dynamodb_local_java_library_path => node[:dynamodb_local][:java_library_path],
     :dynamodb_local_jar => node[:dynamodb_local][:jar],
     :dynamodb_local_db_path => node[:dynamodb_local][:db_path]
