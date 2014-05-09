b5d0d45a395922a9b2d33d80227ca95df4684fbf
Maggie Nelson <maggie@maggienelson.com>
2014-05-09 15:02:28 -0700

    Stripping components on this archive is not required. Set to 0 as ark will otherwise default to 1.

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,6 +2,7 @@ node.default[:dynamodb_local][:source] = 'http://dynamodb-local.s3-website-us-we
 node.default[:dynamodb_local][:port] = 8000
 node.default[:dynamodb_local][:log_dir] = './main'
 node.default[:dynamodb_local][:path] = '/usr/local/dynamodb-local'
+node.default[:dynamodb_local][:strip_components] = 0
 node.default[:dynamodb_local][:java_library_path] = '/usr/local/dynamodb-local/DynamoDBLocal_lib'
 node.default[:dynamodb_local][:jar] = '/usr/local/dynamodb-local/DynamoDBLocal.jar'
 node.default[:dynamodb_local][:db_path] = '/data/dynamodb-local'
